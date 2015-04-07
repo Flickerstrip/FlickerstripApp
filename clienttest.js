7a3a8e94f90c6bceba6e973dcd963c55241444fe
Julian Hartline <julianh2o@gmail.com>
2015-04-06 18:32:47 -0600

    dummy client listens for announcement and connects via tcp

--- clienttest.js
+++ clienttest.js
@@ -1,9 +1,31 @@
 var dgram = require("dgram");
+var net = require('net');
 
 var server = dgram.createSocket('udp4');
 
-server.on('message', function (message) {
+server.on('message', function (message,info) {
     console.log('received a message: ' + message);
+    console.log("type",typeof message);
+    var matches = String(message).match(/PORT:([0-9]+)/);
+    var tcpport = matches[1];
+    var host = info.address;
+    console.log("got port: ",tcpport);
+
+    server.close();
+
+    var client = new net.Socket();
+    client.connect(tcpport, host, function() {
+        console.log('Connected via tcp');
+        client.write('Hello, server! Love, Client.');
+    });
+     
+    client.on('data', function(data) {
+        console.log('Received: ' + data);
+    });
+     
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
 });
 
 var port = 2836;
--- communication.js
+++ communication.js
@@ -3,6 +3,7 @@ var _ = require("underscore")._;
 var util = require("util");
 var fs = require("fs");
 var dgram = require('dgram'); 
+var net = require('net');
 
 var This = function() {
     console.log("initting comms");
@@ -10,12 +11,15 @@ var This = function() {
 };
 
 $.extend(This.prototype,{
+    clients:[],
     init:function() {
-        //-->                                                     freq,multicast addr   ,sprt,dprt
-        this.announcementServer = this.startAnnouncementBroadcast(3000,"255.255.255.255",1836,2836);
-        this.startServer();
+        var tcpport = 3836;
+        var message = "Announcement.. PORT:"+tcpport;
+        //-->                                                     message,freq,multicast addr   ,sprt,dprt
+        this.announcementServer = this.startAnnouncementBroadcast(message,3000,"255.255.255.255",1836,2836);
+        this.startServer(tcpport);
     },
-    startAnnouncementBroadcast:function(frequency,ip,sport,dport) {
+    startAnnouncementBroadcast:function(message,frequency,ip,sport,dport) {
         var server = dgram.createSocket("udp4"); 
 
         server.bind(sport,function() {
@@ -24,15 +28,31 @@ $.extend(This.prototype,{
         });
 
         setInterval(function() {
-            var message = "Announcement broadcast"; //TODO server info?
             server.send(message, 0, message.length, dport, ip);
             console.log("Sent " + message + " to the wire...");
         }, frequency);
 
         return server;
     },
-    startServer:function() {
+    startServer:function(port) {
+        var server = net.createServer(_.bind(function (socket) {
+            socket.name = socket.remoteAddress + ":" + socket.remotePort;
+            
+            this.clients.push(socket);
 
+            console.log("client connected",socket.name);
+
+            socket.on('data', this.receivedClientData);
+            socket.on('end',_.bind(function () {
+                console.log("client disconnected",socket.name);
+                this.clients.splice(this.clients.indexOf(socket), 1);
+            },this));
+        },this)).listen(port);
+
+        return server;
+    },
+    receivedClientData:function(data) {
+        console.log("recevived client data: ",String(data));
     },
     getVisibleStrips:function() {
 		return [
